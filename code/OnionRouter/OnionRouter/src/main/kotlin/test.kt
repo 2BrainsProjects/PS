70cfefa630721abf4c44090a4ccaa6d8bc8b598b
joana <A49469@gmail.com>
2024-03-20 18:11:10 +0000

    demo cliente

---
+++ code/OnionRouter/OnionRouter/src/main/kotlin/test.kt
@@ -0,0 +1,24 @@
+import java.net.InetSocketAddress
+import java.nio.ByteBuffer
+import java.nio.channels.ServerSocketChannel
+import java.nio.channels.SocketChannel
+
+
+fun main(){
+    println("started")
+    val socketChannel = SocketChannel.open()
+    socketChannel.connect(InetSocketAddress(8080))
+    val lastClient = ServerSocketChannel.open()
+    lastClient.socket().bind(InetSocketAddress(8082))
+    val b = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
+    socketChannel.write(b.put("hello||127.0.0.1:8082".toByteArray(Charsets.UTF_8)))
+    println("sent msg")
+
+    val finalClientBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
+    val socket = lastClient.accept()
+
+    println("reading")
+    socket.read(finalClientBuffer)
+    finalClientBuffer.flip()
+    println(Charsets.UTF_8.decode(finalClientBuffer).toString())
+}
