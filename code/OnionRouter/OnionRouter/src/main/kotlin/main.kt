04dad6b59dd4da820eab3be362eeccdc0e52d6c5
joana <A49469@gmail.com>
2024-03-22 16:50:59 +0000

    Debug (code is not Working)

--- code/OnionRouter/OnionRouter/src/main/kotlin/main.kt
+++ code/OnionRouter/OnionRouter/src/main/kotlin/main.kt
@@ -17,10 +17,8 @@ fun main() {
     require(port >= 0) { "Port must not be negative" }
 
     val sSocket = ServerSocketChannel.open()
-    val selector = Selector.open()
-    sSocket.bind(InetSocketAddress(port))
-    sSocket.configureBlocking(false)
-    sSocket.register(selector, SelectionKey.OP_ACCEPT)
+    val cSelector = Selector.open()
+    sSocket.socket().bind(InetSocketAddress(port))
     val sockets = emptyList<SocketChannel>().toMutableList()
 
 //    while (true) {
@@ -45,52 +43,52 @@ fun main() {
 //        }
 //    }
     try {
-        sSocket.bind(InetSocketAddress(port))
-        sSocket.configureBlocking(false)
-        sSocket.register(selector, SelectionKey.OP_ACCEPT)
         println("passing to thread")
         Thread{
-            handleConnection(selector, sockets)
+            handleConnection(cSelector, sockets)
         }.start()
         while(true){
             val clientSocket = sSocket.accept()
             clientSocket.configureBlocking(false)
-            clientSocket.register(selector, SelectionKey.OP_READ)
+            clientSocket.register(cSelector, SelectionKey.OP_READ)
             sockets.add(clientSocket)
+            cSelector.wakeup()
         }
 
     } catch(e: IOException) {
         println(e.message)
     } finally {
-        finalizeOnionRouter(sSocket, selector)
+        finalizeOnionRouter(sSocket, cSelector)
     }
 }
 
 private fun handleConnection(selector: Selector, socketsList: MutableList<SocketChannel>) {
     while (true){
-
-        println("oioioioioioioioioi")
         var readyToRead = selector.select()
-        println(readyToRead)
-        println("oioioioioioioioioi")
 
         if(readyToRead == 0) continue
-        val keys = selector.keys()
+        val keys = selector.selectedKeys()
+        println(keys.size)
         val iterator = keys.iterator()
 
-        while(iterator.hasNext()){
+        while(iterator.hasNext()) {
             val key = iterator.next()
-            if(key.isReadable){
+            iterator.remove()
+
+            if (key.isReadable) {
                 val client = key.channel() as SocketChannel
                 val buffer = ByteBuffer.allocate(1024)
-                var msg= ""
-                while(client.read(buffer) >= 0){
+                var msg = ""
+                while (client.read(buffer) > 0) {
                     buffer.flip()
                     msg += Charsets.UTF_8.decode(buffer).toString()
                     buffer.clear()
                 }
+
+                println("Processing msg: $msg")
                 processMessage(msg, socketsList)
 
+                client.close()
                 if (--readyToRead == 0) break
             }
         }
@@ -100,52 +98,56 @@ private fun handleConnection(selector: Selector, socketsList: MutableList<Socket
 //                          onion
 // adicionar o porto aos routers na db ou aumentar o tamanho do ip
 private fun processMessage(msg:String, socketsList: MutableList<SocketChannel>){
-    val keyPath = System.getProperty("user.dir") + "\\key"
-    // leitura da chave privada
-    val keyStream = FileInputStream(keyPath)
-    val keyBytes = keyStream.readBytes()
-    val privateKeySpec = PKCS8EncodedKeySpec(keyBytes)
-    val keyFactory = KeyFactory.getInstance("RSA")
-    val privateKey = keyFactory.generatePrivate(privateKeySpec)
-
-    val cipher = Cipher.getInstance("RSA/CBC/PKCS1Padding")
-    cipher.init(Cipher.DECRYPT_MODE, privateKey)
-
-    // decifrar a mensagem
-    val decryptedMsg = cipher.doFinal(msg.toByteArray())
-
-    val plaintext = decryptedMsg.decodeToString()
+    if(msg.isBlank() || msg.isEmpty()) return
+//    val keyPath = System.getProperty("user.dir") + "\\key"
+//    // leitura da chave privada
+//    val keyStream = FileInputStream(keyPath)
+//    val keyBytes = keyStream.readBytes()
+//    val privateKeySpec = PKCS8EncodedKeySpec(keyBytes)
+//    val keyFactory = KeyFactory.getInstance("RSA")
+//    val privateKey = keyFactory.generatePrivate(privateKeySpec)
+//
+//    val cipher = Cipher.getInstance("RSA/CBC/PKCS1Padding")
+//    cipher.init(Cipher.DECRYPT_MODE, privateKey)
+//
+//    // decifrar a mensagem
+//    val decryptedMsg = cipher.doFinal(msg.toByteArray())
+//
+    val plaintext = msg //decryptedMsg.decodeToString()
     val addr = plaintext.split("||").last() // onion || 234 325 345 234:4363
     val newMsg = plaintext.dropLastWhile { it != '|' }.dropLast(2)
     var newMsgBytes = newMsg.toByteArray(Charsets.UTF_8)
 
     // verfificar se existe/estabelecer ligação ao nexNode
-    if(!socketsList.any { it.remoteAddress.toString() == addr }){
-        val nextNode = SocketChannel.open()
+    if(!socketsList.any { it.remoteAddress.toString().drop(1) == addr }){
         val splittedAddr = addr.split(':')
-        nextNode.socket().bind(InetSocketAddress(splittedAddr[0], splittedAddr[1].toInt()))
-
+        println(addr)
+        val newAddr = InetSocketAddress(splittedAddr[0], splittedAddr[1].toInt())
+        val nextNode = SocketChannel.open(newAddr)
         socketsList.add(nextNode)
     }
 
-    // socket com o próximo node
-    val socket = socketsList.first { it.remoteAddress.toString() == addr }
+    // socket com o próximo node                           removing prefix '/'  e.g. /127.0.0.1
+    val socket = socketsList.first { it.remoteAddress.toString().drop(1) == addr }
 
     val byteBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
     var msgSize = newMsgBytes.size
     while (msgSize > 0){
-        val chunk = newMsgBytes.copyOf(DEFAULT_BUFFER_SIZE)
+        // minimum size between msgSize and DEFAULT_BUFFER_SIZE
+        val min = minOf(msgSize, DEFAULT_BUFFER_SIZE)
+        val chunk = newMsgBytes.copyOf(min)
         byteBuffer.put(chunk)
         msgSize -= chunk.size
-        newMsgBytes = newMsgBytes.drop(DEFAULT_BUFFER_SIZE).toByteArray()
-    }
+        newMsgBytes = newMsgBytes.drop(min).toByteArray()
+        byteBuffer.flip()
 
-    // enviar onion2
-    socket.write(byteBuffer)
+        // enviar onion2
+        socket.write(byteBuffer)
+        byteBuffer.clear()
+    }
 }
 
 private fun finalizeOnionRouter(sSocket: ServerSocketChannel, selector: Selector){
-    sSocket.close()
     val keys = selector.keys()
     val iterator = keys.iterator()
     while(iterator.hasNext()){
@@ -154,4 +156,5 @@ private fun finalizeOnionRouter(sSocket: ServerSocketChannel, selector: Selector
         client.close()
     }
     selector.close()
+    sSocket.close()
 }
--- code/OnionRouter/OnionRouter/src/main/kotlin/test.kt
+++ code/OnionRouter/OnionRouter/src/main/kotlin/test.kt
@@ -5,22 +5,24 @@ import java.nio.channels.SocketChannel
 
 fun main(){
     println("started")
-    val socketChannel = SocketChannel.open()
-    socketChannel.connect(InetSocketAddress(8080))
+    val socketChannel = SocketChannel.open(InetSocketAddress(8080))
     val lastClient = ServerSocketChannel.open()
     lastClient.socket().bind(InetSocketAddress(8082))
-    val socket = lastClient.accept()
 
-    val b = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
-    socketChannel.write(b.put("hello||127.0.0.1:8082".toByteArray(Charsets.UTF_8)))
-    println("sent msg")
-    socketChannel.close()
+    val output = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
+    output.put ("hello||127.0.0.1:8082".toByteArray())
+    output.flip()    // reset the buffer position to forward data
+    socketChannel.write(output)
 
     val finalClientBuffer = ByteBuffer.allocate(DEFAULT_BUFFER_SIZE)
 
     println("reading")
+    val socket = lastClient.accept()
     socket.read(finalClientBuffer)
     finalClientBuffer.flip()
     println(Charsets.UTF_8.decode(finalClientBuffer).toString())
+
+    lastClient.close()
     socket.close()
+    socketChannel.close()
 }
