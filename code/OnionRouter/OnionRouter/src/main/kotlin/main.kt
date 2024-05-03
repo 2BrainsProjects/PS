90f4dcc448806daf64349598c5f5e9dc77d416e6
49449 - Diogo Almeida <a49449@alunos.isel.pt>
2024-05-03 11:00:32 +0100

    testing without padding error

--- code/OnionRouter/OnionRouter/src/main/kotlin/main.kt
+++ code/OnionRouter/OnionRouter/src/main/kotlin/main.kt
@@ -1,5 +1,28 @@
-import java.net.InetSocketAddress
+import testing.CertificateDomain
 
 fun main() {
-    OnionRouter(InetSocketAddress("127.0.0.1", 8081)).start()
+    val crypto = Crypto()
+    val certificateDomain = CertificateDomain()
+
+    val routerId = 8
+    val port = 8081
+    val msg = "hello"
+
+    val csr = crypto.generateClientCSR(port, "router", "password")
+
+    val certPath = certificateDomain.createCertCommand(csr.joinToString("\n"), routerId, System.getProperty("user.dir") + "\\crypto")
+
+    val certContent = certificateDomain.readFile(certPath)
+
+    val cert = crypto.buildCertificate(certContent)
+
+    val encrypher = crypto.encipher(msg, cert)
+
+    println(encrypher)
+
+    val decrypher = crypto.decipher(encrypher, port)
+
+    println(decrypher)
+
+    // OnionRouter(InetSocketAddress("127.0.0.1", 8081)).start()
 }
---
+++ code/OnionRouter/OnionRouter/src/main/kotlin/testing/CertificateDomain.kt
@@ -0,0 +1,94 @@
+package testing
+
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+
+class CertificateDomain {
+    /**
+     * this function requires openssl installed in system's PATH.
+     * the resultant certificate will be in clientId.crt
+     * @param clientCSR user's certificate signing request
+     * @param clientId user's id
+     * @param path path of the certificate
+     * @return the path of the certificate
+     */
+    fun createCertCommand(
+        clientCSR: String,
+        clientId: Int,
+        path: String,
+    ): String {
+        val csrFile = createCSRTempFile(clientId, clientCSR, path)
+
+        val certFile = File("$path/$clientId.cer")
+
+        if (certFile.exists()) certFile.delete()
+        certFile.createNewFile()
+
+        val signedCertificateCommand = signedCertificateCommand(clientId, path)
+        execute(signedCertificateCommand)
+
+        // wait for the file to be written on
+        while (BufferedReader(FileInputStream(certFile).bufferedReader()).readLines().isEmpty()) {}
+        csrFile.delete()
+
+        return "$path/$clientId.cer"
+    }
+
+    private fun execute(command: String) {
+        try {
+            val process =
+                ProcessBuilder(command.split(" "))
+                    .redirectErrorStream(true)
+                    .start()
+
+            process.waitFor()
+        } catch (e: Exception) {
+            throw IllegalStateException(e.message)
+        }
+    }
+
+    fun readFile(filePath: String): String {
+        val bufferedReader = BufferedReader(FileInputStream(filePath).bufferedReader())
+        val text = bufferedReader.readText()
+        bufferedReader.close()
+        return text
+    }
+
+    private fun createCSRTempFile(
+        clientId: Int,
+        clientCSR: String,
+        path: String,
+    ): File {
+        val dir = File(path)
+        if (!dir.exists()) {
+            dir.mkdir()
+        }
+        val file = File("$path/$clientId.csr")
+        if (!file.exists()) {
+            file.createNewFile()
+            BufferedWriter(OutputStreamWriter(FileOutputStream("$path/$clientId.csr"))).use {
+                it.write("-----BEGIN CERTIFICATE REQUEST-----\n")
+                it.write(clientCSR + "\n")
+                it.write("-----END CERTIFICATE REQUEST-----\n")
+            }
+        }
+        return file
+    }
+
+    private fun signedCertificateCommand(
+        clientId: Int,
+        path: String,
+    ): String =
+        "openssl x509 -req -days 365 -in $path/$clientId.csr -CA $SERVER_PATH/certificate.crt -CAkey $SERVER_PATH/privateKey.key -out $path/$clientId.cer"
+
+    companion object {
+        private val SERVER_PATH
+            get() = path()
+
+        private fun path() = System.getProperty("user.dir") + "\\certificates"
+    }
+}
