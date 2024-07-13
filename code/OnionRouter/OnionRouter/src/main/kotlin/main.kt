00f3587fcc2a4500f811d5fbd9b1d2db0aff27e8
49469 - Joana Chuco <101283731+49469@users.noreply.github.com>
2024-07-13 18:01:29 +0100

    Update main.kt
    Co-Authored-By: Diogo Almeida <77116842+wartuga@users.noreply.github.com>

--- code/OnionRouter/OnionRouter/src/main/kotlin/main.kt
+++ code/OnionRouter/OnionRouter/src/main/kotlin/main.kt
@@ -2,10 +2,9 @@ import java.net.InetSocketAddress
 
 fun main(args: Array<String>) {
     require(args.size == 1) { "Usage: main <port>" }
-    val ip = args[0]
-    val port = args[1].toIntOrNull()
+    val port = args[0].toIntOrNull()
     requireNotNull(port) { "Port must be a number" }
     require(port != 8080) { "Port 8080 is reserved" }
    //val port = 80811
-    OnionRouter(InetSocketAddress(ip, port)).start()
+    OnionRouter(InetSocketAddress("127.0.0.1", port)).start()
 }
